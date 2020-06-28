d823e4c67155bea0e9cb71a3911f8c030828afc8
afaddoul <faddoulachraf39@gmail.com>
2020-06-28 19:55:33 +0100

    generate data_by_regions json file

--- json_update/data_by_country.py
+++ json_update/data_by_country.py
@@ -1,123 +1,129 @@
-import csv, json
+import csv
+import json
 import os
 from pprint import pprint
 from operator import itemgetter
 
-#set json file name
+# set json file name
 json_file = '../json_output/data_by_country.json'
 
 # set CSSE dir path
 csse_dir = '/Users/q-beast/Desktop/covidata-Automation/COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
 
-#set file path
+# set file path
 confirmed_file = csse_dir + 'time_series_covid19_confirmed_global.csv'
 deaths_file = csse_dir + 'time_series_covid19_deaths_global.csv'
 recovered_file = csse_dir + 'time_series_covid19_recovered_global.csv'
 
-#init data lists
+# init data lists
 confirmed_data = []
 deaths_data = []
 recovered_data = []
 
-#read csv files
+# read csv files
 with open(confirmed_file) as f:
-	for row in csv.DictReader(f):
-		confirmed_data.append(row)
+    for row in csv.DictReader(f):
+        confirmed_data.append(row)
 
 with open(deaths_file) as f:
-	for row in csv.DictReader(f):
-		deaths_data.append(row)
+    for row in csv.DictReader(f):
+        deaths_data.append(row)
 
 with open(recovered_file) as f:
-	for row in csv.DictReader(f):
-		recovered_data.append(row)
+    for row in csv.DictReader(f):
+        recovered_data.append(row)
 
-#read json file
+# read json file
 with open(json_file) as f:
-  json_countries = json.load(f)
+    json_countries = json.load(f)
 
 #**************************CONFIRMED CASES PARSING*****************************#
 
-#Get confirmed data for each country
+# Get confirmed data for each country
 confirmed_data_algeria = json.dumps(confirmed_data[2])
 confirmed_data_tunisia = json.dumps(confirmed_data[212])
 confirmed_data_egypt = json.dumps(confirmed_data[98])
 confirmed_data_mauritania = json.dumps(confirmed_data[156])
 confirmed_data_libya = json.dumps(confirmed_data[239])
 
-#load confirmed data for each country
+# load confirmed data for each country
 confirmed_data_algeria = json.loads(confirmed_data_algeria)
 confirmed_data_tunisia = json.loads(confirmed_data_tunisia)
 confirmed_data_egypt = json.loads(confirmed_data_egypt)
 confirmed_data_mauritania = json.loads(confirmed_data_mauritania)
 confirmed_data_libya = json.loads(confirmed_data_libya)
 
-#get list of keys for confirmed data
+# get list of keys for confirmed data
 algeria_lst = list(map(itemgetter(0), confirmed_data_algeria.items()))
 tunisia_lst = list(map(itemgetter(0), confirmed_data_tunisia.items()))
 egypt_lst = list(map(itemgetter(0), confirmed_data_egypt.items()))
 mauritania_lst = list(map(itemgetter(0), confirmed_data_mauritania.items()))
 libya_lst = list(map(itemgetter(0), confirmed_data_libya.items()))
 
-#get last key of confirmed data
+# get last key of confirmed data
 algeria_last_key = algeria_lst[-1]
 tunisia_last_key = tunisia_lst[-1]
 egypt_last_key = egypt_lst[-1]
 mauritania_key = mauritania_lst[-1]
 libya_last_key = libya_lst[-1]
 
-#get last updated value of confirmed cases
+# get last updated value of confirmed cases
 confirmed_cases_algeria = confirmed_data_algeria[algeria_last_key]
 confirmed_cases_tunisia = confirmed_data_tunisia[tunisia_last_key]
 confirmed_cases_egypt = confirmed_data_egypt[egypt_last_key]
 confirmed_cases_mauritania = confirmed_data_mauritania[mauritania_key]
 confirmed_cases_libya = confirmed_data_libya[libya_last_key]
 
-#override old values
-json_countries['data']['countries'][0]['confirmed'] = int(confirmed_cases_algeria)
-json_countries['data']['countries'][1]['confirmed'] = int(confirmed_cases_tunisia)
-json_countries['data']['countries'][2]['confirmed'] = int(confirmed_cases_egypt)
-json_countries['data']['countries'][3]['confirmed'] = int(confirmed_cases_mauritania)
-json_countries['data']['countries'][4]['confirmed'] = int(confirmed_cases_libya)
+# override old values
+json_countries['data']['countries'][0]['confirmed'] = int(
+    confirmed_cases_algeria)
+json_countries['data']['countries'][1]['confirmed'] = int(
+    confirmed_cases_tunisia)
+json_countries['data']['countries'][2]['confirmed'] = int(
+    confirmed_cases_egypt)
+json_countries['data']['countries'][3]['confirmed'] = int(
+    confirmed_cases_mauritania)
+json_countries['data']['countries'][4]['confirmed'] = int(
+    confirmed_cases_libya)
 
 #**************************DEATHS CASES PARSING*****************************#
 
-#Get deaths data for each country
+# Get deaths data for each country
 deaths_data_algeria = json.dumps(deaths_data[2])
 deaths_data_tunisia = json.dumps(deaths_data[212])
 deaths_data_egypt = json.dumps(deaths_data[98])
 deaths_data_mauritania = json.dumps(deaths_data[156])
 deaths_data_libya = json.dumps(deaths_data[239])
 
-#load deaths data for each country
+# load deaths data for each country
 deaths_data_algeria = json.loads(deaths_data_algeria)
 deaths_data_tunisia = json.loads(deaths_data_tunisia)
 deaths_data_egypt = json.loads(deaths_data_egypt)
 deaths_data_mauritania = json.loads(deaths_data_mauritania)
 deaths_data_libya = json.loads(deaths_data_libya)
 
-#get list of keys for deaths data
+# get list of keys for deaths data
 algeria_lst = list(map(itemgetter(0), deaths_data_algeria.items()))
 tunisia_lst = list(map(itemgetter(0), deaths_data_tunisia.items()))
 egypt_lst = list(map(itemgetter(0), deaths_data_egypt.items()))
 mauritania_lst = list(map(itemgetter(0), deaths_data_mauritania.items()))
 libya_lst = list(map(itemgetter(0), deaths_data_libya.items()))
 
-#get last key of deaths data
+# get last key of deaths data
 algeria_last_key = algeria_lst[-1]
 tunisia_last_key = tunisia_lst[-1]
 egypt_last_key = egypt_lst[-1]
 mauritania_key = mauritania_lst[-1]
 libya_last_key = libya_lst[-1]
 
-#get last updated value of deaths cases
+# get last updated value of deaths cases
 deaths_cases_algeria = deaths_data_algeria[algeria_last_key]
 deaths_cases_tunisia = deaths_data_tunisia[tunisia_last_key]
 deaths_cases_egypt = deaths_data_egypt[egypt_last_key]
 deaths_cases_mauritania = deaths_data_mauritania[mauritania_key]
 deaths_cases_libya = deaths_data_libya[libya_last_key]
 
-#override old values
+# override old values
 json_countries['data']['countries'][0]['deaths'] = int(deaths_cases_algeria)
 json_countries['data']['countries'][1]['deaths'] = int(deaths_cases_tunisia)
 json_countries['data']['countries'][2]['deaths'] = int(deaths_cases_egypt)
@@ -126,49 +132,55 @@ json_countries['data']['countries'][4]['deaths'] = int(deaths_cases_libya)
 
 #**************************RECOVERED CASES PARSING*****************************#
 
-#Get recovered data for each country
+# Get recovered data for each country
 recovered_data_algeria = json.dumps(recovered_data[2])
 recovered_data_tunisia = json.dumps(recovered_data[212])
 recovered_data_egypt = json.dumps(recovered_data[90])
 recovered_data_mauritania = json.dumps(recovered_data[152])
 recovered_data_libya = json.dumps(recovered_data[143])
 
-#load recovered data for each country
+# load recovered data for each country
 recovered_data_algeria = json.loads(recovered_data_algeria)
 recovered_data_tunisia = json.loads(recovered_data_tunisia)
 recovered_data_egypt = json.loads(recovered_data_egypt)
 recovered_data_mauritania = json.loads(recovered_data_mauritania)
 recovered_data_libya = json.loads(recovered_data_libya)
 
-#get list of keys for recovered data
+# get list of keys for recovered data
 algeria_lst = list(map(itemgetter(0), recovered_data_algeria.items()))
 tunisia_lst = list(map(itemgetter(0), recovered_data_tunisia.items()))
 egypt_lst = list(map(itemgetter(0), recovered_data_egypt.items()))
 mauritania_lst = list(map(itemgetter(0), recovered_data_mauritania.items()))
 libya_lst = list(map(itemgetter(0), recovered_data_libya.items()))
 
-#get last key of recovered data
+# get last key of recovered data
 recovered_last_key = algeria_lst[-1]
 recovered_last_key = tunisia_lst[-1]
 recovered_last_key = egypt_lst[-1]
 recovered_key = mauritania_lst[-1]
 recovered_key = libya_lst[-1]
 
-#get last updated value of recovered cases
+# get last updated value of recovered cases
 recovered_cases_algeria = recovered_data_algeria[algeria_last_key]
 recovered_cases_tunisia = recovered_data_tunisia[tunisia_last_key]
 recovered_cases_egypt = recovered_data_egypt[egypt_last_key]
 recovered_cases_mauritania = recovered_data_mauritania[mauritania_key]
 recovered_cases_libya = recovered_data_libya[libya_last_key]
 
-#override old values
-json_countries['data']['countries'][0]['recovered'] = int(recovered_cases_algeria)
-json_countries['data']['countries'][1]['recovered'] = int(recovered_cases_tunisia)
-json_countries['data']['countries'][2]['recovered'] = int(recovered_cases_egypt)
-json_countries['data']['countries'][3]['recovered'] = int(recovered_cases_mauritania)
-json_countries['data']['countries'][4]['recovered'] = int(recovered_cases_libya)
+# override old values
+json_countries['data']['countries'][0]['recovered'] = int(
+    recovered_cases_algeria)
+json_countries['data']['countries'][1]['recovered'] = int(
+    recovered_cases_tunisia)
+json_countries['data']['countries'][2]['recovered'] = int(
+    recovered_cases_egypt)
+json_countries['data']['countries'][3]['recovered'] = int(
+    recovered_cases_mauritania)
+json_countries['data']['countries'][4]['recovered'] = int(
+    recovered_cases_libya)
 
-#create and write json output file
+# create and write json output file
 f = open("output.json", "w")
-json_countries = json.dump(json_countries, f, indent=4, sort_keys=True, ensure_ascii=False)
+json_countries = json.dump(json_countries, f, indent=4,
+                           sort_keys=True, ensure_ascii=False)
 f.close()
---
+++ json_update/data_by_regions.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from pprint import pprint
+import json
+import sys
+sys.path.insert(0, '/Users/q-beast/Desktop/covidata-Automation')
+from scraping_scripts import regions_cases_scraper as regions
+from scraping_scripts import total_cases_scraper as total
+
+
+
+# set json file name
+json_file = '../json_output/data_by_regions.json'
+
+# read json file
+with open(json_file) as f:
+    json_regions = json.load(f)
+
+regions_cases_dict = regions.regions_cases_scraper()
+total_cases_dict = total.total_cases_scraper()
+
+# pprint(total_cases_dict)
+# pprint(json_regions['data'])
+
+# Update total country cases
+json_regions['data']['country'][0]['confirmed'] = total_cases_dict['data']['total_confirmed']
+json_regions['data']['country'][0]['deaths'] = total_cases_dict['data']['total_deaths']
+json_regions['data']['country'][0]['excluded'] = total_cases_dict['data']['total_excluded']
+json_regions['data']['country'][0]['recovered'] = total_cases_dict['data']['total_recovered']
+
+# Update total regions cases
+json_regions['data']['regions'][0]['confirmed'] = regions_cases_dict['data'][0]['confirmed']
+json_regions['data']['regions'][1]['confirmed'] = regions_cases_dict['data'][1]['confirmed']
+json_regions['data']['regions'][2]['confirmed'] = regions_cases_dict['data'][2]['confirmed']
+json_regions['data']['regions'][3]['confirmed'] = regions_cases_dict['data'][3]['confirmed']
+json_regions['data']['regions'][4]['confirmed'] = regions_cases_dict['data'][4]['confirmed']
+json_regions['data']['regions'][5]['confirmed'] = regions_cases_dict['data'][5]['confirmed']
+json_regions['data']['regions'][6]['confirmed'] = regions_cases_dict['data'][6]['confirmed']
+json_regions['data']['regions'][7]['confirmed'] = regions_cases_dict['data'][7]['confirmed']
+json_regions['data']['regions'][8]['confirmed'] = regions_cases_dict['data'][8]['confirmed']
+json_regions['data']['regions'][9]['confirmed'] = regions_cases_dict['data'][9]['confirmed']
+json_regions['data']['regions'][10]['confirmed'] = regions_cases_dict['data'][10]['confirmed']
+json_regions['data']['regions'][11]['confirmed'] = regions_cases_dict['data'][11]['confirmed']
+
+# Update updating time
+update_date = str(total_cases_dict['date']['update_hour']) + ' ' + str(total_cases_dict['date']['update_day']) + '-' + str(datetime.today().month) + '-' + str(datetime.today().year)
+json_regions['last_updated'] = update_date
+
+f = open("output.json", "w")
+json_countries = json.dump(json_regions, f, indent=4, sort_keys=True, ensure_ascii=False)
+f.close()
