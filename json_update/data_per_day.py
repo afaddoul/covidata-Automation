c83ef609215788febec4996b3164f9b82c48f03a
afaddoul <faddoulachraf39@gmail.com>
2020-06-28 20:23:51 +0100

    data_per_day update script done

--- json_update/data_per_day.py
+++ json_update/data_per_day.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import json
 import sys
 sys.path.insert(0, '/Users/q-beast/Desktop/covidata-Automation')
-from scraping_scripts import regions_cases_scraper as regions
+from scraping_scripts import daily_cases_scraper as daily
 from scraping_scripts import total_cases_scraper as total
 
 # set json file name
@@ -13,7 +13,13 @@ json_file = '../json_output/data_per_day.json'
 with open(json_file) as f:
     json_days = json.load(f)
 
-regions_cases_dict = regions.regions_cases_scraper()
-total_cases_dict = total.total_cases_scraper()
+daily_cases_dict = daily.daily_cases_scraper()
 
-pprint(json_days)
+pub_date = daily_cases_dict['date']['update_day'] + '/' + str(datetime.today().month) + '/' + str(datetime.today().year)[2:]
+
+last_updated_elm = {'deaths': daily_cases_dict['data']['daily_deaths'], 'pub_date': pub_date, 'recovered': daily_cases_dict['data']['daily_recovered']}
+json_days['data'].append(last_updated_elm)
+
+f = open("output.json", "w")
+json_countries = json.dump(json_days, f, indent=4, sort_keys=True, ensure_ascii=False)
+f.close()
--- scraping_scripts/daily_cases_scraper.py
+++ scraping_scripts/daily_cases_scraper.py
@@ -19,12 +19,10 @@ def daily_cases_scraper():
     lst = []
     for row in rows:
         row = row.text.replace("i>", "").split()[0]
-        print(row, '\n')
         if (row.isnumeric()):
             lst.append(int(row))
         else:
             lst.append(0)
-    print(lst)
     # Get total Update date
     rows = soup.findAll('span', attrs={'class': 'font-13'})
     row = rows[0].text.split()
@@ -44,5 +42,3 @@ def daily_cases_scraper():
         }
     }
     return daily_properties
-
-pprint(daily_cases_scraper())
